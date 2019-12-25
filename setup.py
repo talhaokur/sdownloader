64389b298ead3f20619579fff385bc1dffbca5a2
Talha Okur <t@talhaokur.net>
2019-12-26 00:52:49 +0300

    Setup script updated

--- setup.py
+++ setup.py
@@ -1,19 +1,27 @@
 from setuptools import setup, find_packages
 
-setup(name="suite_downloader",
+setup(name="sdownloader",
       version="0.1.0",
       description="A little program to download given list of matrices from SuiteSparse.",
-      url="https://github.com/talhaokur/suite_downloader",
+      url="https://github.com/talhaokur/sdownloader",
       author="Talha Okur",
       author_email="talhao@acm.org",
       license="MIT",
-      packages=find_packages(where='./'),
+      packages=find_packages(),
       install_requires=[
           "requests",
       ],
       python_requires=">=3",
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "Development Status :: 2 - Pre-Alpha",
+          "Environment :: Console",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+          "Intended Audience :: End Users/Desktop",
+      ],
       entry_points={
           'console_scripts': [
-              'suite_downloader=src.main:main',],
+              'sdownloader=src.main:main',],
               },
       )
