8247a057d6553ac72d3dd834e1405be3e084f597
Talha Okur <t@talhaokur.net>
2019-12-28 00:32:02 +0300

    Internet connection test added

--- src/fetch.py
+++ src/fetch.py
@@ -3,6 +3,7 @@ import os
 import logging
 import tarfile
 import shutil
+from . import utils
 
 
 SOURCE_URL = "https://sparse.tamu.edu/"
@@ -10,21 +11,26 @@ SOURCE_URL = "https://sparse.tamu.edu/"
 
 def read_matrix_list_file(file_path):
     """
-    
+
     """
     if not os.path.exists(file_path):
         logging.critical("List file couldn't found exiting...")
         raise SystemExit()
-    
+
     matrices = []
 
     with open(file_path) as list_file:
         content = list_file.read().splitlines()
-        
+
     matrices = [tuple(line.split('/')) for line in content]
     return matrices
 
+
 def download_data_files(matrix_list, destination=None, keep_archive_files=False):
+    if not utils.check_internet_connection():
+        logging.critical("Can not connect to the Internet")
+        raise SystemExit("Can not connect to the Internet, please check your connection. Exiting...")
+
     if not destination:
         destination = './'
         logging.info("`destination` paramater isn't provided. Downloading to `./`")
@@ -53,10 +59,11 @@ def download_data_files(matrix_list, destination=None, keep_archive_files=False)
         shutil.rmtree('./archive')
         logging.info('Archive files deleted.')
 
+
 def extract_archive_files(file_list, destination=None):
     for f in file_list:
         with tarfile.open(f, "r:gz") as tar:
             tar.extractall(path=destination)
             logging.debug("`" + f + '` extracted to ' + destination)
-    
+
     logging.info("Archive files are extracted to: " + destination)
