7150ae0cbcdaca50debf7671ce7d428ddcd8a2f8
Talha Okur <t@talhaokur.net>
2019-12-28 22:59:02 +0300

    Internet connection check added
    Also a little typo fixed

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 import argparse
 import logging
 import os
-from . import fetch
+from . import fetch, utils
 
 def main():
     parser = argparse.ArgumentParser(
-        prog="ssdownloader", 
+        prog="sdownloader", 
         description="Data downloader from SparseSuite.")
     parser.add_argument("-f", "--list-file", help="matrix name list file destination.", required=True)
     parser.add_argument("-d", "--destination", help="destination path.", default=os.getcwd())
@@ -17,7 +17,13 @@ def main():
 
     logging.basicConfig(level = log_level, format="[%(asctime)s]:%(levelname)s:%(message)s", datefmt='%H:%M:%S')
 
+    if not utils.check_internet_connection():
+        logging.critical("Can not connect to the Internet")
+        raise SystemExit("Can not connect to the Internet, please check your connection. Exiting...")
+
+    utils.check_version()
     matrices = fetch.read_matrix_list_file(args.list_file)
     fetch.download_data_files(matrices, destination=args.destination, keep_archive_files=False)
 
-main()
+if __name__ == "__main__":
+    main()
